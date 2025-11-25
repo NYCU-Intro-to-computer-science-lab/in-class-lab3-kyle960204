2446d1d366ecd3a487c4fd78db93caf833b1d17c
kyle960204 <163109035+kyle960204@users.noreply.github.com>
2025-11-25 13:45:53 +0800

    Update p1.cpp

--- src/p1.cpp
+++ src/p1.cpp
@@ -1,5 +1,3 @@
-// 題目 1：高塔建築師 (The Tower Architect) - 20分
-
 #include <iostream>
 using namespace std;
 long long factorial(int n){
@@ -13,11 +11,12 @@ long long sum(int n){
   if(n==1||n==0){
        return n;
    }
-   return n+factorial(n-1);
+   return n+sum(n-1);
+}
 int main() {
     int n;
     cin>>n;
-    cout<<factorial(n);
+    cout<<factorial(n)<<endl;
     cout<<sum(n);
     return 0;
 }
